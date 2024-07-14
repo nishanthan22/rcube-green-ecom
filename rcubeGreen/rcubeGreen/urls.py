11bbaf4adb759b219fde5e4fee11232ec6f553ac
NavjotNavj <navjots8867@gmail.com>
2024-07-14 14:01:16 -0400

    1) Created forms, views and urls for model classes. 2) template for GreenInnovation model. 3) Base.css

--- rcubeGreen/rcubeGreen/urls.py
+++ rcubeGreen/rcubeGreen/urls.py
@@ -17,11 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include('allauth.urls')),
     path("", include('usersApp.urls')),
     path('', include('rcubeApp.urls')),
-]
+    path('', include('blog.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
