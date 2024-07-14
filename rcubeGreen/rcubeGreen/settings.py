6952eb94063a352e5bbe795f148752ade72f2f2d
Singhmeghna <48406875+Singhmeghna@users.noreply.github.com>
2024-07-13 21:04:49 -0400

    View for Add Payment Method

--- rcubeGreen/rcubeGreen/settings.py
+++ rcubeGreen/rcubeGreen/settings.py
@@ -133,6 +133,11 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Additional static files settings
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
