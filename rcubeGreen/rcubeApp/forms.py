e9e82480df32c84286d8c5de64c5cf393a0dfdf2
Singhmeghna <48406875+Singhmeghna@users.noreply.github.com>
2024-07-13 21:15:34 -0400

    Form for Payment Method

--- rcubeGreen/rcubeApp/forms.py
+++ rcubeGreen/rcubeApp/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Payment
+from .models import Payment, PaymentMethod
 
 class PaymentForm(forms.ModelForm):
     class Meta:
         model = Payment
         fields = ['amount', 'payment_method']
+
+class PaymentMethodForm(forms.ModelForm):
+    class Meta:
+        model = PaymentMethod
+        fields = ['method_name', 'details']
+
