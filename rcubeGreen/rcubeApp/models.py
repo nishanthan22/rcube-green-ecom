c5bfa362f078c6c767bab2ede92cbabc7f3146f3
Singhmeghna <48406875+Singhmeghna@users.noreply.github.com>
2024-07-13 21:10:45 -0400

    Model for Payment

--- rcubeGreen/rcubeApp/models.py
+++ rcubeGreen/rcubeApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class NewsArticle(models.Model):
@@ -17,3 +18,13 @@ class NewsArticle(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=100)
+    payment_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount}"
