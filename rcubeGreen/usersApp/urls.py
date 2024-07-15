a37fb83f980137d80da0700a42f09bb1b468fec7
Sasmita <sasmitab88@gmail.com>
2024-07-15 16:02:33 -0400

    Product & Categories Edit Features

--- rcubeGreen/usersApp/urls.py
+++ rcubeGreen/usersApp/urls.py
@@ -7,9 +7,14 @@ from . import views
 
 urlpatterns = [
     path("", views.user_login),
-    path("logout", views.user_logout),
-    path('add_category/', views.add_category, name='add_category'),
-    path('categories/', views.list_categories, name='list_categories'),
-    path('add_product/', views.add_product, name='add_product'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('products/add/', views.add_product, name='add_product'),
     path('products/', views.list_products, name='list_products'),
+    path('edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('categories/add/', views.add_category, name='add_category'),
+    path('categories/', views.list_categories, name='list_categories'),
+    path('edit_category/<int:category_id>/', views.edit_category, name='edit_category'),
+    path('delete_category/<int:category_id>/', views.delete_category, name='delete_category'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- rcubeGreen/usersApp/views.py
+++ rcubeGreen/usersApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from .forms import ProductForm, CategoryForm
@@ -32,6 +32,25 @@ def list_products(request):
     products = Product.objects.all()
     return render(request, 'list_products.html', {'products': products})
 
+# Product Views
+def edit_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('list_products')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'edit_product.html', {'form': form})
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('list_products')
+    return render(request, 'delete_product.html', {'product': product})
+
 
 #@login_required
 def add_category(request):
@@ -49,3 +68,22 @@ def add_category(request):
 def list_categories(request):
     categories = Category.objects.all()
     return render(request, 'list_categories.html', {'categories': categories})
+
+# Category Views
+def edit_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('list_categories')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'edit_category.html', {'form': form})
+
+def delete_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('list_categories')
+    return render(request, 'delete_category.html', {'category': category})
