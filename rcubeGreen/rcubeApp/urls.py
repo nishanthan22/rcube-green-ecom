ec4a9cf4be74b3d8df9a6ec6c0e50161e0a5216b
Singhmeghna <48406875+Singhmeghna@users.noreply.github.com>
2024-07-14 21:42:42 -0400

    Both the Funcationalities of Payment Method + Payment History. Needs some improvements. Once User does the Payment , he gets redirected to his Payment History.

--- rcubeGreen/rcubeApp/urls.py
+++ rcubeGreen/rcubeApp/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('search/', views.search, name='search'),
     path('add-payment-method/', views.add_payment_method, name='add_payment_method'),
-    path('payment-history/', views.payment_history, name='payment_history'),
+    path('payment-history/', views.payment_history, name='payment_list'),
     path('export-payments-to-csv/', views.export_payments_to_csv, name='export_payments_to_csv'),
 ]
