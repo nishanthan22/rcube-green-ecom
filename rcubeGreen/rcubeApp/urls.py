4b31427f36fa64e5b86b321e93490131bd9178c3
Prithvika Babu <prithvika2111@gmail.com>
2024-07-22 22:42:41 -0400

    Search bar with all functionalities added

--- rcubeGreen/rcubeApp/urls.py
+++ rcubeGreen/rcubeApp/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('add-payment-method/', views.add_payment_method, name='add_payment_method'),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('payment-successful/<int:payment_id>/', views.payment_successful, name='payment_successful'),
+    path('product/<int:id>/', views.product_detail, name='product_detail'),
 
 ]
